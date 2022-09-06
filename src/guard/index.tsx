1f6033f9a03db8c897dca47f9819c452bb7f2df9
lucaperullo <lucaperullo@outlook.it>
2022-09-06 10:32:37 +0200

    path fix

--- src/guard/index.tsx
+++ src/guard/index.tsx
@@ -1,6 +1,6 @@
 const authorise = async (setUser: (arg0: any) => void) => {
   try {
-    const response = await fetch(`${import.meta.env.VITE_BASE_URL}/users/me`, {
+    const response = await fetch(`${import.meta.env.VITE_BASE_URL}users/me`, {
       credentials: "include",
     });
     const data = await response.json();
