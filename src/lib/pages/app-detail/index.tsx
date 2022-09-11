72f99f17e18d1f602c48a1fa3645f83e8e2f04df
lucaperullo <lucaperullo@outlook.it>
2022-09-12 01:35:10 +0200

    fix

--- src/lib/pages/app-detail/index.tsx
+++ src/lib/pages/app-detail/index.tsx
@@ -50,122 +50,120 @@ export default function AppDetail() {
   }, [path, id]);
 
   return (
-    app !== null && (
-      <Container
-        maxW={{
-          base: "100%",
-          md: "7xl",
+    <Container
+      maxW={{
+        base: "100%",
+        md: "7xl",
+      }}
+      p={0}
+    >
+      <Flex
+        bg="gray.100"
+        position="sticky"
+        p={{
+          base: "0",
+          md: "4",
+          lg: "8",
         }}
-        p={0}
+        py="4"
+        top="20"
+        zIndex={2}
+        borderBottom="1px solid #e2e8f0"
+        _dark={{
+          bg: "gray.900",
+          borderBottom: "1px solid #2d3748",
+        }}
+        justifyContent="space-between"
+        alignItems="center"
       >
-        <Flex
-          bg="gray.100"
-          position="sticky"
-          p={{
-            base: "0",
-            md: "4",
-            lg: "8",
+        {" "}
+        <IconButton
+          mr="8"
+          ml="4"
+          size={"lg"}
+          aria-label="go back"
+          icon={<ArrowBackIcon />}
+          onClick={() => {
+            window.history.back();
           }}
-          py="4"
-          top="20"
-          zIndex={2}
-          borderBottom="1px solid #e2e8f0"
-          _dark={{
-            bg: "gray.900",
-            borderBottom: "1px solid #2d3748",
-          }}
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          {" "}
-          <IconButton
-            mr="8"
-            ml="4"
-            size={"lg"}
-            aria-label="go back"
-            icon={<ArrowBackIcon />}
-            onClick={() => {
-              window.history.back();
+        />
+        <Flex justifyContent={"space-between"} w="full" flexDir="column">
+          <Text fontSize={22} fontWeight={700}>
+            {app?.name}
+          </Text>
+          <Text
+            fontSize={{
+              base: "sm",
+              md: "md",
+              lg: "xl",
             }}
-          />
-          <Flex justifyContent={"space-between"} w="full" flexDir="column">
-            <Text fontSize={22} fontWeight={700}>
-              {app?.name}
-            </Text>
-            <Text
-              fontSize={{
-                base: "sm",
-                md: "md",
-                lg: "xl",
-              }}
-              fontWeight={100}
-            >
-              {path}
-            </Text>
-          </Flex>
-        </Flex>
-        <SimpleGrid
-          columns={{ base: 1, lg: 2 }}
-          spacing={{ base: 8, md: 10 }}
-          py={{ base: 18, md: 24 }}
-        >
-          <Flex justifyContent="center">
-            <Box
-              bgImage={`url(${app?.image})`}
-              bgSize="cover"
-              bgPosition="center"
-              w={{ base: "100px", sm: "300px", lg: "400px" }}
-              h={{ base: "100px", sm: "300px", lg: "400px" }}
-            />
-          </Flex>
-          <Button
-            mx="4"
-            position={{ base: "unset", md: "sticky" }}
-            top="60"
-            as="a"
-            href={app?.url}
-            target="_blank"
-            rounded={"none"}
-            w={"auto"}
-            mt={8}
-            size={"sm"}
-            py={"7"}
-            bg={useColorModeValue("gray.900", "gray.50")}
-            color={useColorModeValue("white", "gray.900")}
-            textTransform={"uppercase"}
-            _hover={{
-              transform: "translateY(2px)",
-              boxShadow: "lg",
-            }}
-            rightIcon={<ArrowForwardIcon w={4} h={4} />}
+            fontWeight={100}
           >
-            Visit {app?.name}
-          </Button>
-          <LikeButton element={app} showCount={true} />
-          <Stack spacing={{ base: 6, md: 10 }}>
-            <Stack
-              spacing={{ base: 4, sm: 6 }}
-              direction={"column"}
-              divider={
-                <StackDivider
-                  borderColor={useColorModeValue("gray.200", "gray.600")}
-                />
-              }
-            >
-              <VStack spacing={{ base: 4, sm: 6 }}>
-                <Text
-                  p="4"
-                  color={useColorModeValue("gray.500", "gray.400")}
-                  fontSize={"2xl"}
-                  fontWeight={"300"}
-                >
-                  {app?.description}
-                </Text>
-              </VStack>
-            </Stack>
+            {path}
+          </Text>
+        </Flex>
+      </Flex>
+      <SimpleGrid
+        columns={{ base: 1, lg: 2 }}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 18, md: 24 }}
+      >
+        <Flex justifyContent="center">
+          <Box
+            bgImage={`url(${app?.image})`}
+            bgSize="cover"
+            bgPosition="center"
+            w={{ base: "100px", sm: "300px", lg: "400px" }}
+            h={{ base: "100px", sm: "300px", lg: "400px" }}
+          />
+        </Flex>
+        <Button
+          mx="4"
+          position={{ base: "unset", md: "sticky" }}
+          top="60"
+          as="a"
+          href={app?.url}
+          target="_blank"
+          rounded={"none"}
+          w={"auto"}
+          mt={8}
+          size={"sm"}
+          py={"7"}
+          bg={useColorModeValue("gray.900", "gray.50")}
+          color={useColorModeValue("white", "gray.900")}
+          textTransform={"uppercase"}
+          _hover={{
+            transform: "translateY(2px)",
+            boxShadow: "lg",
+          }}
+          rightIcon={<ArrowForwardIcon w={4} h={4} />}
+        >
+          Visit {app?.name}
+        </Button>
+        <LikeButton element={app} showCount={true} />
+        <Stack spacing={{ base: 6, md: 10 }}>
+          <Stack
+            spacing={{ base: 4, sm: 6 }}
+            direction={"column"}
+            divider={
+              <StackDivider
+                borderColor={useColorModeValue("gray.200", "gray.600")}
+              />
+            }
+          >
+            <VStack spacing={{ base: 4, sm: 6 }}>
+              <Text
+                p="4"
+                color={useColorModeValue("gray.500", "gray.400")}
+                fontSize={"2xl"}
+                fontWeight={"300"}
+              >
+                {app?.description}
+              </Text>
+            </VStack>
           </Stack>
-        </SimpleGrid>
-      </Container>
-    )
+        </Stack>
+      </SimpleGrid>
+    </Container>
   );
 }
