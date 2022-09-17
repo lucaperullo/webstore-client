79e5b6d31592c8459a5d773611add96d148b537a
lucaperullo <lucaperullo@outlook.it>
2022-09-17 18:35:15 +0200

    new loader

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ const App = () => {
   return (
     <ChakraProvider theme={theme}>
       <Box>
-        {state.isLoading && <Loader />}
+        <Loader />
 
         <Router>
           <Navbar>
--- src/lib/loader/index.tsx
+++ src/lib/loader/index.tsx
@@ -1,7 +1,8 @@
-import { Box, Spinner, Text } from "@chakra-ui/react";
+import { Box, Flex, Spinner, Text } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import { useEffect } from "react";
 import { useStateValue } from "../../context/stateProvider";
-
+import "./style.css";
 export default function Loader() {
   const [{ isLoading }, dispatch] = useStateValue();
   useEffect(() => {
@@ -22,6 +23,7 @@ export default function Loader() {
       position="fixed"
       zIndex={999}
       w="100%"
+      flexDir="column"
       overflow="hidden"
       display="flex"
       justifyContent="center"
@@ -29,13 +31,20 @@ export default function Loader() {
       bg="rgba(0,0,0,0.4)"
       backdropFilter={"blur(2px)"}
     >
-      <Box
+      {/* <Box
         display="flex"
         justifyContent="center"
         alignItems="center"
         flexDir="column"
         p={8}
-        borderRadius="md"
+        borderRadius="full"
+        bg={"rgba(0,0,0,0.3)"}
+        backdropFilter={"blur(12px)"}
+        _dark={{
+          bg: "rgba(255,255,255,0.3)",
+        }}
+        h="140px"
+        w="140px"
       >
         <Spinner
           size="xl"
@@ -49,10 +58,46 @@ export default function Loader() {
           _dark={{
             color: "gray.100",
           }}
+          fontWeight={700}
         >
           Loading{" "}
         </Text>
-      </Box>
+      </Box> */}
+      {/* create animation where 6 apps goes into 1 box in loop  */}
+      <div className="container">
+        <div className="dot dot-1"></div>
+        <div className="dot dot-2"></div>
+        <div className="dot dot-3"></div>
+      </div>
+
+      <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation="10"
+              result="blur"
+            />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 21 -7"
+            />
+          </filter>
+        </defs>
+      </svg>
+      <Flex alignItems="baseline">
+        <Text
+          fontSize={{
+            base: "md",
+            md: "lg",
+            lg: "xl",
+          }}
+          fontWeight={700}
+        >
+          Loading
+        </Text>
+      </Flex>
     </Box>
   );
 }
